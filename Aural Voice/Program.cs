1ac4ffcc133c2c08fb8bf68a489cbb00c01228ca
Eivind <58335786+eico6@users.noreply.github.com>
2022-08-21 21:52:46 +0200

    Startup Structure Test

---
+++ Aural Voice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aural_Voice
+{
+    static class Program
+    {
+        /// <summary>
+        /// Welcome to Aural Voice!
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        static void Testen()
+        {
+
+        }
+    }
+}
