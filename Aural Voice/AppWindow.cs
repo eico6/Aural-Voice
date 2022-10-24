f618d02de5901c395012c254d401e67f52140c25
Eivind <58335786+eico6@users.noreply.github.com>
2022-10-24 19:17:23 +0200

    ResourceManager and PianoAudio structure
    - I would prefer to be able to embed the '.wav' files into the 'PianoAudio.resx', but I get an error regarding serialization. So for now at least, linking it at compile time should suffice.
    - Full error message (same message for all '.wav' files): "Type 'System.IO.MemoryStream' in Assembly 'System.Private.CoreLib, Version=6.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' is not marked as serializable."

--- Aural Voice/AppWindow.cs
+++ Aural Voice/AppWindow.cs
@@ -1,16 +1,17 @@
 using System.Media;
-using System.Reflection;
-using System.Resources;
 
 namespace AuralVoice;
 
 
 public partial class AppWindow : Form
 {
+    // Use multi-threading for more responsive feedback
+    // - https://learn.microsoft.com/en-us/dotnet/desktop/winforms/controls/multithreading-in-windows-forms-controls?view=netframeworkdesktop-4.8
+    // - https://learn.microsoft.com/en-us/dotnet/desktop/winforms/controls/how-to-load-a-sound-asynchronously-within-a-windows-form?view=netframeworkdesktop-4.8
+
     internal AppWindow()
     {
         InitializeComponent();
-
     }
 
     private void AppWindow_Load(object sender, EventArgs e)
@@ -20,20 +21,14 @@ public partial class AppWindow : Form
 
     private void button1_Click(object sender, EventArgs e)
     {
-        // Use multi-threading for more responsive feedback
-        // - https://learn.microsoft.com/en-us/dotnet/desktop/winforms/controls/multithreading-in-windows-forms-controls?view=netframeworkdesktop-4.8
-        // - https://learn.microsoft.com/en-us/dotnet/desktop/winforms/controls/how-to-load-a-sound-asynchronously-within-a-windows-form?view=netframeworkdesktop-4.8
-
-        SoundPlayer soundPlayer = new SoundPlayer(@"C:\Visual Studio Projects\Aural-Voice\Aural Voice\resources\audio\C5.wav");
+        // Notice that we need more threads to play these simultaneously
+        SoundPlayer soundPlayer = new SoundPlayer(resources.PianoAudio.test);
+        SoundPlayer soundPlayer2 = new SoundPlayer(resources.PianoAudio.C5);
+        soundPlayer2.Play();
         soundPlayer.Play();
 
 
-        //if (_soundPlayer != null && _audioPath != null)
-        //{
-        //    _soundPlayer.Play();
-        //    MessageBox.Show($"You hit the note C5. \n\nNow playing audio from: {_audioPath}");
-        //}
+        // gjør da inni ein thorw cath, eller exception kanskje
 
-        
     }
 }
