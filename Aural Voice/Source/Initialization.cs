4c4ff8b46b53ef46781576e0af46cd1566fef0b7
Eivind <58335786+eico6@users.noreply.github.com>
2022-11-15 17:57:48 +0100

    Folder Structure - Source

---
+++ Aural Voice/Source/Initialization.cs
@@ -0,0 +1,153 @@
+﻿using MaterialSkin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Preview.Notes;
+using Windows.Devices.Midi;
+
+namespace AuralVoice;
+
+partial class AppWindow
+{
+    /// <summary>
+    ///  Used as an alias to keep note names consistent and fail-safe.
+    /// </summary>
+    internal static class NoteName
+    {
+        // Octave 0
+        internal const String A0  = "A0";
+        internal const String Bb0 = "Bb0";
+        internal const String B0  = "B0";
+
+        // Octave 1
+        internal const String C1  = "C1";
+        internal const String Db1 = "Db1";
+        internal const String D1  = "D1";
+        internal const String Eb1 = "Eb1";
+        internal const String E1  = "E1";
+        internal const String F1  = "F1";
+        internal const String Gb1 = "Gb1";
+        internal const String G1  = "G1";
+        internal const String Ab1 = "Ab1";
+        internal const String A1  = "A1";
+        internal const String Bb1 = "Bb1";
+        internal const String B1  = "B1";
+
+        // Octave 2
+        internal const String C2  = "C2";
+        internal const String Db2 = "Db2";
+        internal const String D2  = "D2";
+        internal const String Eb2 = "Eb2";
+        internal const String E2  = "E2";
+        internal const String F2  = "F2";
+        internal const String Gb2 = "Gb2";
+        internal const String G2  = "G2";
+        internal const String Ab2 = "Ab2";
+        internal const String A2  = "A2";
+        internal const String Bb2 = "Bb2";
+        internal const String B2  = "B2";
+
+        // Octave 3
+        internal const String C3  = "C3";
+        internal const String Db3 = "Db3";
+        internal const String D3  = "D3";
+        internal const String Eb3 = "Eb3";
+        internal const String E3  = "E3";
+        internal const String F3  = "F3";
+        internal const String Gb3 = "Gb3";
+        internal const String G3  = "G3";
+        internal const String Ab3 = "Ab3";
+        internal const String A3  = "A3";
+        internal const String Bb3 = "Bb3";
+        internal const String B3  = "B3";
+
+        // Octave 4
+        internal const String C4  = "C4";
+        internal const String Db4 = "Db4";
+        internal const String D4  = "D4";
+        internal const String Eb4 = "Eb4";
+        internal const String E4  = "E4";
+        internal const String F4  = "F4";
+        internal const String Gb4 = "Gb4";
+        internal const String G4  = "G4";
+        internal const String Ab4 = "Ab4";
+        internal const String A4  = "A4";
+        internal const String Bb4 = "Bb4";
+        internal const String B4  = "B4";
+
+        // Octave 5
+        internal const String C5  = "C5";
+        internal const String Db5 = "Db5";
+        internal const String D5  = "D5";
+        internal const String Eb5 = "Eb5";
+        internal const String E5  = "E5";
+        internal const String F5  = "F5";
+        internal const String Gb5 = "Gb5";
+        internal const String G5  = "G5";
+        internal const String Ab5 = "Ab5";
+        internal const String A5  = "A5";
+        internal const String Bb5 = "Bb5";
+        internal const String B5  = "B5";
+
+        // Octave 6
+        internal const String C6  = "C6";
+        internal const String Db6 = "Db6";
+        internal const String D6  = "D6";
+        internal const String Eb6 = "Eb6";
+        internal const String E6  = "E6";
+        internal const String F6  = "F6";
+        internal const String Gb6 = "Gb6";
+        internal const String G6  = "G6";
+        internal const String Ab6 = "Ab6";
+        internal const String A6  = "A6";
+        internal const String Bb6 = "Bb6";
+        internal const String B6  = "B6";
+
+        // Octave 7
+        internal const String C7  = "C7";
+        internal const String Db7 = "Db7";
+        internal const String D7  = "D7";
+        internal const String Eb7 = "Eb7";
+        internal const String E7  = "E7";
+        internal const String F7  = "F7";
+        internal const String Gb7 = "Gb7";
+        internal const String G7  = "G7";
+        internal const String Ab7 = "Ab7";
+        internal const String A7  = "A7";
+        internal const String Bb7 = "Bb7";
+        internal const String B7  = "B7";
+
+        // Octave 8
+        internal const String C8  = "C8";
+    }
+
+    /// <summary>
+    ///  Adds 88 notes to the piano, and assigns a unique reference 
+    ///  between each "note" and its corresponding "key".
+    /// </summary> 
+    private void InitializePiano()
+    {
+        piano = new Piano();
+        
+        piano.AddNote(NoteName.A0, keyA0);
+        piano.AddNote(NoteName.Bb0, keyBb0);
+        // ...
+
+        // TODO: at the end, throw exception unless '_notes.count()' == 88
+    }
+
+    /// <summary>
+    ///  WinForms theme: MaterialSkin 2, by leocb.
+    /// </summary> 
+    private void InitializeMaterialSkin()
+    {
+        materialSkinManager = MaterialSkinManager.Instance;
+        materialSkinManager.AddFormToManage(this);
+        materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+        materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey700, Primary.BlueGrey900, Primary.BlueGrey500, 
+                                                          Accent.LightBlue200, TextShade.WHITE);
+    }
+}
---
+++ Aural Voice/Source/Note.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Devices.Midi;
+using Windows.UI.Notifications;
+
+namespace AuralVoice;
+
+/// <summary>
+///  Contained in Piano and recieves input from the associated key.
+/// </summary>
+internal class Note : Piano
+{
+    private readonly String? _name;
+    private readonly PictureBox _associatedKey;
+    private readonly byte _midiIndex;
+    private readonly bool _isBlack;
+    private IMidiMessage? _midiMessage;
+
+    internal Note(in String noteName, ref PictureBox keyRef)
+    {
+        _name = noteName;
+        _associatedKey = keyRef;
+        _midiIndex = GetMidiIndex();
+        _isBlack = _associatedKey.Name.Contains('b') ? true : false;
+    }
+
+    internal void PlayNote()
+    {
+        _midiMessage = new MidiNoteOnMessage(defaultChannel, _midiIndex, maxVelocity);
+
+        if (midiDevice != null)
+        {
+            midiDevice.SendMessage(_midiMessage);
+        }
+    }
+
+    internal void StopNote()
+    {
+        _midiMessage = new MidiNoteOffMessage(defaultChannel, _midiIndex, maxVelocity);
+
+        if (midiDevice != null)
+        {
+            midiDevice.SendMessage(_midiMessage);
+        }
+    }
+
+    /// <summary>
+    ///  Handles visual and audio output of a key/note based on <paramref name="keyAction"/>.
+    /// </summary>
+    internal void KeyInput(in KeyAction keyAction)
+    {
+        KeyStatus keyStatus;
+
+        switch (keyAction)
+        {
+            case KeyAction.ENTER:
+                keyStatus = KeyStatus.HOVER;
+                break;
+            case KeyAction.LEAVE:
+                keyStatus = KeyStatus.IDLE;
+                break;
+            case KeyAction.DOWN:
+                keyStatus = KeyStatus.PRESS;
+                PlayNote();
+                break;
+            case KeyAction.UP:
+                keyStatus = KeyStatus.HOVER;
+                StopNote();
+                break;
+            default:
+                // exception
+                keyStatus=KeyStatus.IDLE;
+                break;
+        }
+
+        SetKeyImage(keyStatus);
+    }
+
+    /// <summary>
+    ///  Sets the displayed image of the associated key to its corresponding <paramref name="keyStatus"/>..
+    /// </summary>
+    private void SetKeyImage(in KeyStatus keyStatus)
+    {
+        switch (keyStatus)
+        {
+            case KeyStatus.IDLE:
+                _associatedKey.Image = _isBlack ? _keyImages["idle_black"] : _keyImages["idle_white"];
+                break;
+            case KeyStatus.HOVER:
+                _associatedKey.Image = _isBlack ? _keyImages["hover_black"] : _keyImages["hover_white"];
+                break;
+            case KeyStatus.PRESS:
+                _associatedKey.Image = _isBlack ? _keyImages["press_black"] : _keyImages["press_white"];
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    ///  Calculates and returns a midi message note index based on 'Note._name'.
+    /// </summary>
+    private byte GetMidiIndex()
+    {
+        /// <Summary>
+        /// Midi message note index:
+        /// - Index range  (0 - 127)
+        /// - Lowest note  (21 = A0)
+        /// - Highest note (108 = C8)
+        /// 
+        ///  Return: 12 + (sum of each char in 'this._name'), where their values are the following:
+        ///  '0' = 0     'C' = 0    
+        ///  '1' = 12    'D' = 2    
+        ///  '2' = 24    'E' = 4    
+        ///  '3' = 36    'F' = 5    
+        ///  '4' = 48    'G' = 7    
+        ///  '5' = 60    'A' = 9
+        ///  '6' = 72    'B' = 11   
+        ///  '7' = 84    'b' = -1
+        ///  '8' = 96
+        ///  Example: _name = "Eb4" would be equal to (12 + 4 + (-1) + 48) = 63
+        /// </Summary>
+
+        if (_name != null)
+        {
+            byte calculatedIndex = 12;
+
+            for (int i = 0; i < _name.Length; i++)
+            {
+                switch (_name[i])
+                {
+                    case 'C':
+                        calculatedIndex += 0;
+                        break;
+                    case 'D':
+                        calculatedIndex += 2;
+                        break;
+                    case 'E':
+                        calculatedIndex += 4;
+                        break;
+                    case 'F':
+                        calculatedIndex += 5;
+                        break;
+                    case 'G':
+                        calculatedIndex += 7;
+                        break;
+                    case 'A':
+                        calculatedIndex += 9;
+                        break;
+                    case 'B':
+                        calculatedIndex += 11;
+                        break;
+                    case 'b':
+                        calculatedIndex -= 1;
+                        break;
+                    case '0':
+                        calculatedIndex += 0;
+                        break;
+                    case '1':
+                        calculatedIndex += 12;
+                        break;
+                    case '2':
+                        calculatedIndex += 24;
+                        break;
+                    case '3':
+                        calculatedIndex += 36;
+                        break;
+                    case '4':
+                        calculatedIndex += 48;
+                        break;
+                    case '5':
+                        calculatedIndex += 60;
+                        break;
+                    case '6':
+                        calculatedIndex += 72;
+                        break;
+                    case '7':
+                        calculatedIndex += 84;
+                        break;
+                    case '8':
+                        calculatedIndex += 96;
+                        break;
+                    default:
+                        // Exception: invalid note name! Char '_name[i]' is not accounted for.
+                        break;
+                }
+            }
+
+            return calculatedIndex;
+        }
+
+        // Exception: this_name == null
+        return 0;
+    }
+}
